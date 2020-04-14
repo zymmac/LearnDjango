1beba86d34d4f31dcb7cc947f8ed44cf98026313
Alexandre Costa <zymmac@gmail.com>
2020-04-13 22:58:52 -0300

    Created base.html and extended with Django

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from main_app import views
 
+# SET THE NAMESPACE!
+app_name = 'main_app'
+
 urlpatterns = [
-    path('', views.shiba, name='shiba'),
+    path('shiba/', views.shiba, name='shiba'),
+    path('form/', views.formpage, name='form')
 ]
